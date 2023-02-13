647943b446cfbf4ffa9e4a52b1556571a33d3f32
gsesek <destined19876@gmail.com>
2023-02-13 13:19:05 +0100

    13/2/23 - Added comments for each step of the first test 13/2/23 - Printing the name of the page (headless mode)

--- src/test/java/FirstTestExample.java
+++ src/test/java/FirstTestExample.java
@@ -4,6 +4,7 @@ import com.microsoft.playwright.Playwright;
 
 public class FirstTestExample {
     public static void main(String[] args) {
+        String link = "https://rahulshettyacademy.com/AutomationPractice/";
         //Creating Playwright instance
         Playwright playwright = Playwright.create();
         //From Playwright instance we need to create browser
@@ -11,6 +12,8 @@ public class FirstTestExample {
         //From browser we are creating new page
         Page page = browser.newPage();
         //From the page we navigate to any URL of choice
-
+        page.navigate(link);
+        //Retrieving the page title
+        System.out.println(page.title());
     }
 }
