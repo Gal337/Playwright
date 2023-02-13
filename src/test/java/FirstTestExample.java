9cd605c5df26fdc5ea4eef43e91c61104eda40c1
gsesek <destined19876@gmail.com>
2023-02-13 13:25:48 +0100

    13/2/23 - Added comments for each step of the first test 13/2/23 - Printing the name of the page (headless mode) 13/2/23 - Closing the session at the end

--- src/test/java/FirstTestExample.java
+++ src/test/java/FirstTestExample.java
@@ -1,14 +1,15 @@
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 
 public class FirstTestExample {
     public static void main(String[] args) {
-        String link = "https://rahulshettyacademy.com/AutomationPractice/";
+        String link = "https://way2automation.com";
         //Creating Playwright instance
         Playwright playwright = Playwright.create();
-        //From Playwright instance we need to create browser
-        Browser browser = playwright.chromium().launch();
+        //From Playwright instance we need to create browser //Adding new Browser object with headless mode set to false
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
         //From browser we are creating new page
         Page page = browser.newPage();
         //From the page we navigate to any URL of choice
