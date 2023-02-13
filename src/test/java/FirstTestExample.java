101b2ef7d3e3c8006aeca3b00bb9e7744f7248f1
gsesek <destined19876@gmail.com>
2023-02-13 13:28:48 +0100

    13/2/23 - Added command to maximize browser window

--- src/test/java/FirstTestExample.java
+++ src/test/java/FirstTestExample.java
@@ -10,6 +10,8 @@ public class FirstTestExample {
         Playwright playwright = Playwright.create();
         //From Playwright instance we need to create browser //Adding new Browser object with headless mode set to false
         Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        //Command for maximizing the browser window
+        browser.newContext(new Browser.NewContextOptions().setViewportSize(1920,1080))
         //From browser we are creating new page
         Page page = browser.newPage();
         //From the page we navigate to any URL of choice
