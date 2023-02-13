03ee72555da946e69ca115bd101efbef85c16bdb
gsesek <destined19876@gmail.com>
2023-02-13 12:38:41 +0100

    13/2/23 - Added Playwright dependancy to pom.xml 13/2/23 - Added comments for each step 13/2/23 -

--- src/test/java/FirstTestExample.java
+++ src/test/java/FirstTestExample.java
@@ -1,8 +1,16 @@
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 
 public class FirstTestExample {
     public static void main(String[] args) {
+        //Creating Playwright instance
         Playwright playwright = Playwright.create();
-        playwright.chromium().launch();
+        //From Playwright instance we need to create browser
+        Browser browser = playwright.chromium().launch();
+        //From browser we are creating new page
+        Page page = browser.newPage();
+        //From the page we navigate to any URL of choice
+
     }
 }
